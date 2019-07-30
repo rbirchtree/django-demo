f229dbde39c359328397cbe2724d21f525d7ec91
Rob Birch <rbirchtree@gmail.com>
2019-07-29 18:55:09 -0700

    start of video 44

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from realtors.models import Realtor
+from listings.models import Listing
+
 # Create your views here.
 def index(request):
-    return render(request,'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings' : listings
+    }
+
+    return render(request,'pages/index.html',context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors' : realtors,
+        'mvp_realtors' : mvp_realtors
+    }
+
+    return render(request,'pages/about.html', context)
 
 def rob(request):
     return render(request,'pages/rob.html')
--- realtors/admin.py
+++ realtors/admin.py
@@ -6,7 +6,7 @@ from .models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id','name','email','hire_date')
     list_display_links = ('id','name')
-    search_fields = ('name')
+    search_fields = ('name',)
     list_per_page = 25
-    
-admin.site.register(Realtor)
+
+admin.site.register(Realtor, RealtorAdmin)
